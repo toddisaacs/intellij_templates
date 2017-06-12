78a80c98ee6fa52e63c66487e3bb2eade42a7f7c
Todd Isaacs <teisaacs21w@gmail.com>
2017-06-12 12:53:15 -0400

    Create react_stateless_component_wit_props.js

---
+++ react_stateless_component_wit_props.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const $NAME$ = (props) => {
+  return (
+    <div>
+      
+    </div>
+  );
+};
+
+$NAME$.propTypes = {
+  myProp: PropTypes.string.isRequired,
+};
+
+export default $NAME$;
